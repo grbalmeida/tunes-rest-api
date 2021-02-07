9219fc2062a53abe275b025ad2bd169aa4455417
gilvan <g.r.almeida@live.com>
2021-02-07 18:57:26 -0300

    Gerando arquivo excel na controller de faixas

---
+++ Tunes/Tunes.Api/Excel/FaixaColumns.cs
@@ -0,0 +1,14 @@
+﻿namespace Tunes.Api.Excel
+{
+    public enum FaixaColumns
+    {
+        Nome = 1,
+        Compositor = 2,
+        Album = 3,
+        TipoMidia = 4,
+        Genero = 5,
+        Milissegundos = 6,
+        Bytes = 7,
+        PrecoUnitario = 8
+    }
+}
--- Tunes/Tunes.Api/V2/Controllers/FaixaController.cs
+++ Tunes/Tunes.Api/V2/Controllers/FaixaController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tunes.Api.Controllers;
+using Tunes.Api.Excel;
 using Tunes.Api.ViewModels;
 using Tunes.Business.CollectionFilters;
 using Tunes.Business.Interfaces;
@@ -32,6 +34,50 @@ namespace Tunes.Api.V2.Controllers
         public async Task<IList<FaixaViewModel>> GetAll([FromQuery] FaixaFiltro filtro)
         {
             return _mapper.Map<IList<FaixaViewModel>>(await _faixaRepository.Filtro(filtro));
+        }
+
+        [HttpGet]
+        [Route("excel")]
+        public async Task<IActionResult> Excel([FromQuery] FaixaFiltro filtro)
+        {
+            var faixas = await _faixaRepository.Filtro(filtro);
+
+            using (var wb = new XLWorkbook())
+            {
+                var ws = wb.AddWorksheet("Faixas");
+                var linhaAtual = 1;
+                ws.Cell(linhaAtual, (int)FaixaColumns.Nome).Value = "Nome";
+                ws.Cell(linhaAtual, (int)FaixaColumns.Nome).Style.Font.Bold = true;
+                ws.Cell(linhaAtual, (int)FaixaColumns.Compositor).Value = "Compositor";
+                ws.Cell(linhaAtual, (int)FaixaColumns.Compositor).Style.Font.Bold = true;
+                ws.Cell(linhaAtual, (int)FaixaColumns.Album).Value = "Álbum";
+                ws.Cell(linhaAtual, (int)FaixaColumns.Album).Style.Font.Bold = true;
+                ws.Cell(linhaAtual, (int)FaixaColumns.TipoMidia).Value = "Tipo de Mídia";
+                ws.Cell(linhaAtual, (int)FaixaColumns.TipoMidia).Style.Font.Bold = true;
+                ws.Cell(linhaAtual, (int)FaixaColumns.Genero).Value = "Gênero";
+                ws.Cell(linhaAtual, (int)FaixaColumns.Genero).Style.Font.Bold = true;
+                ws.Cell(linhaAtual, (int)FaixaColumns.Milissegundos).Value = "Milissegundos";
+                ws.Cell(linhaAtual, (int)FaixaColumns.Milissegundos).Style.Font.Bold = true;
+                ws.Cell(linhaAtual, (int)FaixaColumns.Bytes).Value = "Bytes";
+                ws.Cell(linhaAtual, (int)FaixaColumns.Bytes).Style.Font.Bold = true;
+                ws.Cell(linhaAtual, (int)FaixaColumns.PrecoUnitario).Value = "Preço Unitário";
+                ws.Cell(linhaAtual, (int)FaixaColumns.PrecoUnitario).Style.Font.Bold = true;
+
+                foreach (var faixa in faixas)
+                {
+                    linhaAtual++;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.Nome).Value = faixa.Nome;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.Compositor).Value = faixa.Compositor;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.Album).Value = faixa?.Album?.Titulo;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.TipoMidia).Value = faixa?.TipoMidia?.Nome;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.Genero).Value = faixa?.Genero?.Nome;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.Milissegundos).Value = faixa.Milissegundos;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.Bytes).Value = faixa.Bytes;
+                    ws.Cell(linhaAtual, (int)FaixaColumns.PrecoUnitario).Value = faixa.PrecoUnitario;
+                }
+
+                return await GerarArquivoExcel(wb, "albuns.xlsx");
+            }
         }
     }
 }
